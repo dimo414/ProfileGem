fed2ea850cd8a1243898f62a42666d9129c5d100
Michael Diamond <dimo414@gmail.com>
2020-06-11 17:33:33 -0700

    Remove erroneous quotes, the %q is sufficient to prevent unintended expansions.

--- utilityFunctions.sh
+++ utilityFunctions.sh
@@ -186,7 +186,7 @@ pg::require() {
         return
       fi
 
-      printf '%s not available\n%s\n' "$cmd" "$(printf %q "$msg")" >&2
+      printf '%s not available\n%s\n' "$cmd" $(printf %q "$msg") >&2
       return 127
     }
 EOF
