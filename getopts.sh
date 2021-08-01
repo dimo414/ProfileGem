580cbc8bfc7b37983da7e2baad88571f2d5e2fd7
Michael Diamond <dimo414@gmail.com>
2021-07-31 17:13:41 -0700

    Support numeric arguments in pg::optparse

--- getopts.sh
+++ getopts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Helper utility to simplify using Bash's getopts builtin.
+# Helper utility to simplify using Bash's getopts utility, and make it usable with functions.
 #
 # This is lightly forked from https://gist.github.com/dimo414/93776b78a38791ed1bd1bad082d08009
 # to use ProfileGem's namespace.
@@ -27,6 +27,9 @@
 # set to the value passed as an argument. To check if a (non-empty) argument was passed
 # use [[ -n "$..." ]].
 #
+# Numeric options, such as -0 which is often used to configure NUL-byte delimited output,
+# are also supported but are prefixed with "o" to distinguish from the positional arguments.
+#
 # All parsed arguments are shift-ed out of $@, leaving any subsequent positional arguments
 # in-place. A -- argument can be used to halt option parsing early, e.g. `-a -- -b` will
 # only parse -a and leave -b as an argument.
@@ -38,21 +41,32 @@
 # Parsing errors cause the calling function to return with exit code 2. If a _usage
 # variable is in scope its contents will be included in the error message.
 #
+# The output of this function is intended to be passed to eval, therefore we try to
+# minimize what it prints and delegate most of the heavy lifting to a separate function.
+# Expected output:
+#   local OPTIND=1 a b c=0 # vars are dynamically parsed
+#   _parse_opts_helper a:b:c 0 '' "$@" || return
+#   shift $((OPTIND - 1)); OPTIND=1
 pg::getopts() {
-  local i char last_char vars=() optstring=${1?optstring} min_args=${2:-0} max_args=${3:-}
-  if ! [[ "$optstring" =~ ^[a-zA-Z:]*$ ]] || [[ "$optstring" == *::* ]]; then
+  local i char last_char var vars=() optstring=${1:?optstring} min_args=${2:-0} max_args=${3:-}
+  optstring="${optstring#:}" # ensure string is not prefixed with :
+  if ! [[ "$optstring" =~ ^[a-zA-Z0-9:]*$ ]] || [[ "$optstring" == *::* ]]; then
     pg::err "Invalid optstring: $optstring"
     echo 'return 2' # for eval-ing
     return 2
   fi
-  optstring="${optstring#:}" # ensure string is not prefixed with :
   for (( i=${#optstring}-1 ; i >= 0 ; i-- )); do
     char=${optstring:i:1}
     if [[ "$char" != ":" ]]; then
+      var="$char"
+      if [[ "$var" =~ [0-9] ]]; then
+        # prefix with 'o' so numeric flags aren't confused for positional args
+        var="o${var}"
+      fi
       if [[ "$last_char" == ":" ]]; then
-        vars+=("$char")
+        vars+=("$var")
       else
-        vars+=("${char}=0")
+        vars+=("${var}=0")
       fi
     fi
     last_char=$char
@@ -85,6 +99,9 @@ pg::_getopts_helper() {
       *)
         if [[ "$optstring" != *"${opt}:"* ]]; then
           OPTARG=1
+        fi
+        if [[ "$opt" =~ [0-9] ]]; then
+          opt="o${opt}"
         fi
         printf -v "$opt" '%s' "$OPTARG"
         ;;
